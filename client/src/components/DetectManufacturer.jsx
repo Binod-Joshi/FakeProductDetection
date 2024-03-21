20e6edb27294ae257314b51c50ea47d4823db540
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-03-21 23:34:21 +0530

    detect Manufacturer completed

--- client/src/components/DetectManufacturer.jsx
+++ client/src/components/DetectManufacturer.jsx
@@ -1,14 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DetectManufacturer.css";
+import { ethers } from "ethers";
+import { contractABI, contractAddress } from "../contractDetails/ContractDetails";
+import { Alert } from "@mui/material";
 
 const DetectManufacturer = () => {
   const [address, setAddress] = useState("");
+  const [manufacturedData, setManufacturedData] = useState("");
+  const [status, setStatus] = useState("");
+  const [response, setResponse] = useState("");
 
-  const submitHandler = (e) => {
+  const submitHandler = async(e) => {
     e.preventDefault();
     console.log(address);
+    try {
+      const {ethereum} = window;
+      const provider = new ethers.BrowserProvider(ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(contractAddress,contractABI,signer);
+      const manufacturerData = await contract.checkManufacturer(address);
+      console.log("success");
+      setManufacturedData(manufacturerData);
+      console.log(manufacturedData?.exists);
+      if(!manufacturerData?.exists){
+        setStatus("info");
+        setResponse("Manufacturer Not Found!")
+      }
+    } catch (error) {
+      console.log(error);
+      setStatus("error");
+      setResponse(error?.reverts?.args[0] || "Failed");
+    }
   };
+
+  useEffect(() => {
+    if(status){
+      const timeout = setTimeout(() => {
+        setStatus("");
+        setResponse("");
+      },2000);
+
+      return () => clearTimeout(timeout);
+    }
+  },[status])
   return (
+    <>
     <div className="detectManufacturer">
       <p
         style={{
@@ -37,27 +73,33 @@ const DetectManufacturer = () => {
           onChange={(e) => setAddress(e.target.value)}
         />
       </form>
-      <div className="showManufacturerData">
+      {manufacturedData?.exists && <><div className="showManufacturerData">
         <p className="availableHeader">Manufacturer Available</p>
       </div>
       <div className="showManufacturerData1">
           <div>
             <p className="contentOfAvailableHeader">
-              Manufacturer address: 0x00000000000000000000000
+              Manufacturer address: {manufacturedData?.wallet_address}
             </p>
           </div>
           <div>
             <p className="contentOfAvailableHeader">
-              Manufacturer Name: 
+              Manufacturer Name: {manufacturedData?.name}
             </p>
           </div>
           <div>
             <p className="contentOfAvailableHeader">
-              Manufacturer Website: 
+              Manufacturer Website: {manufacturedData?.website}
             </p>
           </div>
-        </div>
+        </div></>}
     </div>
+    {status !== "" && <div>
+    <div className="alertTop">
+          <Alert severity={status}>{response}</Alert>
+        </div>
+      </div>}
+    </>
   );
 };
 
